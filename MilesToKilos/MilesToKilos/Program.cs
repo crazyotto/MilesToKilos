9523f3dce7f983b77234aa2a2f750e042b1034c8
crazyotto <34284410+crazyotto@users.noreply.github.com>
2018-08-28 12:29:09 -0500

    initial commit

---
+++ MilesToKilos/MilesToKilos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MilesToKilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double kilos = 1.6;
+            Console.Write("Please enter distance in miles: ");
+            double miles = Convert.ToDouble(Console.ReadLine());
+            double result = miles * kilos;
+            Console.WriteLine(miles + " miles is " + result + " kilometers.");
+            Console.ReadLine();
+        }
+    }
+}
